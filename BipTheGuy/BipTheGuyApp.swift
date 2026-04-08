299f111e0265d7de18dfda5d06b411e3ba36c5a0
Kane2suave333 <medinakane3@icloud.com>
2026-04-08 14:20:26 -0700

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by MEDINA, ANTHONY on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
